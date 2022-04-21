28e8f006ddb925cf732e1b88aea08d6754aa60d8
Mr-DecodeBlock <decodeblock1010@gmail.com>
2022-04-21 18:31:50 -0500

    main

---
+++ src/components/secure-transactions/SecureTransaction.js
@@ -0,0 +1,51 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import ReactWOW from "react-wow";
+
+const SecureTransaction = () => {
+  return (
+    <div className="dg__secure__transaction">
+      <div className="container">
+        <div className="row align-items-center">
+
+        <div className="col-lg-6">
+            <ReactWOW duration="2s" animation="fadeInLeft">
+              <div className="dg__secure__thumb">
+                <img
+                  src={process.env.PUBLIC_URL + "/images/about/20.png"}
+                  alt="secure images"
+                />
+              </div>
+            </ReactWOW>
+          </div>
+
+          <div className="col-lg-6">
+            <div className="dg__secure__inner">
+              <h2>
+              The most powerful cryptoactive in the market with passive 
+              income according to your investment.
+              </h2>
+              <p>
+              IliminatisCoin Is a high-frequency trading token-based rewards protocol 
+              developed with the vision of giving you passive rewards according to your investment.
+              </p>
+              <p>
+              Our community-driven protocol combines the concepts of Trading and Defi
+              to produce passive rewards for everyone on an ongoing basis..
+              </p>
+              <Link
+                className="secure__btn dg__btn btn--trans"
+                to={process.env.PUBLIC_URL + "/contact"}
+              >
+                Read More
+              </Link>
+            </div>
+          </div>
+          
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default SecureTransaction;
---
+++ src/pages/HomeFork.js
@@ -0,0 +1,48 @@
+import React, { Fragment } from "react";
+import MetaTags from "react-meta-tags";
+import LayoutOne from "../layouts/LayoutOne";
+import HeroSliderTwo from "../containers/hero-sliders/HeroSliderTwo";
+import CurrencyTicker from "../containers/currency-tickers/CurrencyTicker";
+import AboutContent from "../components/about-contents/AboutContent";
+import EasyStart from "../containers/easy-starts/EasyStart";
+
+import SecureTransaction from "../components/secure-transactions/SecureTransaction";
+import SoftwareDownload from "../components/software-downloads/SoftwareDownload";
+
+import CounterUp from "../containers/counter-ups/CounterUp";
+import CurrencyCalculation from "../components/currency-calculations/CurrencyCalculation";
+
+const HomeOne = () => {
+  return (
+    <Fragment>
+      <MetaTags>
+        <title>IliminuatisCoin | Home</title>
+        <meta
+          name="description"
+          content="Homepage of React JS Crypto Currency Template."
+        />
+      </MetaTags>
+      <LayoutOne>
+        {/* hero slider */}
+        <HeroSliderTwo />
+        {/* currency ticker */}
+        <CurrencyTicker />
+        {/* easy start */}
+        <EasyStart />
+        {/* currency calculation */}
+        <SecureTransaction />
+        {/* live chart */}
+        <CurrencyCalculation />
+        {/* counter up */}
+        <CounterUp backgroundImage="/images/bg/020.jpg" />
+        {/* secure transaction */}
+        <AboutContent />
+        {/* video popup */}
+        <SoftwareDownload />
+        {/* blog grid */}
+      </LayoutOne>
+    </Fragment>
+  );
+};
+
+export default HomeOne;
